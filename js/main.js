a9c72ac7f6a0e52b0225e192964073d710c78c16
Tyler Piazza <tyler_piazza@college.harvard.edu>
2020-11-14 15:33:49 -0500

    dot scrolling, loaded data, skeleton of page done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log("let's get started!");
+// load data using promises
+let promises = [
+    d3.csv("data/faculty_coauthor_matrix.csv"),
+    d3.csv('data/per_paper_vals_v1.csv'),
+    d3.csv('data/Visualization Data_People.csv'),
+    d3.csv('data/Visualization Data_Courses.csv'),
+    d3.csv('data/Visualization Data_News.csv'),
+    d3.csv('data/Visualization Data_Programs.csv')
+    // please add additional data AFTER these, so that people's indexing isn't messed up...
+];
+
+Promise.all(promises)
+    .then( function(data){ initMainPage(data) })
+    .catch( function (err){console.log(err)} );
+
+// initMainPage
+function initMainPage(dataArray) {
+
+    // log data
+    console.log('Check out the data', dataArray);
+
+    // initialize the visualizations here
+    //myMatrixVis = new MatrixVis('matrixDiv', dataArray[0], dataMarriages, dataBusiness)
+}
